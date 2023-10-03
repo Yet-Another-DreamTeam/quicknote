ac9831542f0e2d35636ee0000dee0b95a3e06386
lucaspalomodevelop <lucas.palomo@t-online.de>
2023-10-03 13:56:00 +0200

    add basic commands

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,17 @@
 
 int main(int argc, char *argv[])
 {
-    // command.addCommand("add", "add a note", add);
-    // command.addCommand("list", "list all notes", list);
-    // command.addCommand("view", "remove a note", view);
-    // command.addCommand("edit", "edit a note", edit);
-    // command.addCommand("remove", "remove a note", remove);
-    // command.addCommand("search", "search quicknote", search);
-    // command.addCommand("help", "show help", help);
+    command.addCommand("add", "add a note", commands::add);
+    command.addCommand("list", "list all notes", commands::list);
+    command.addCommand("view", "remove a note", commands::view);
+    command.addCommand("edit", "edit a note", commands::edit);
+    command.addCommand("remove", "remove a note", commands::remove);
+    command.addCommand("search", "search quicknote", commands::search);
+    command.addCommand("help", "show help", commands::help);
+
+    command.execute(argc, argv);
 
     setup.runSetup();
-    std::cout << "quicknote!" << std::endl;
+    // std::cout << "quicknote!" << std::endl;
     return 0;
 }
--- src/main.h
+++ src/main.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "./setup.h"
+#include "./command.h"
+#include "./commands.h"
 
 Setup setup;
-// Command command;
+Command command;
